3c1d75e3352980267ac8d2c448d89a38d7859abe
John Hoar <John.Hoar@esa.int>
2023-05-23 11:34:06 +0200

    Swicth to jpg for size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<div style="background-image: url(/euclidcrop.png); background-size: 50%; background-repeat: no-repeat; background-size: cover;" class="container h-full mx-auto flex justify-center items-center">
+<div style="background-image: url(/euclidcrop.jpg); background-size: 50%; background-repeat: no-repeat; background-size: cover;" class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
 		<h1 class="h1">Euclid!</h1>
 	</div>
