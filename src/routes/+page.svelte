adda3bacbdf44f096f5497c36ef73284f3be8da0
John Hoar <johnhoar@yahoo.co.uk>
2023-05-30 12:37:05 +0200

    start harmonising json schema

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,16 +26,16 @@
 	<div class="imgbckspace-y-5 w-96">
 		<h1 class="h1 text-white flex justify-center py-2">{header[$locale]}</h1>
 		{#each data.appdata.trails as trail}
-			{#each trail.doc as doc} 
-				{#if doc.lang === $locale}
+			{#each trail.data as data} 
+				{#if data.lang === $locale}
 				<div class="card m-2">
-					<header class="card-header variant-filled-tertiary rounded-none rounded-t-lg text-white text-2xl py-3">{(doc.title).toUpperCase()}</header>
+					<header class="card-header variant-filled-tertiary rounded-none rounded-t-lg text-white text-2xl py-3">{(data.title).toUpperCase()}</header>
 					<section class="bg-black">
 						<div>
-							<img src={trail.image} alt={doc.title}/>
+							<img src={trail.image} alt={data.title}/>
 						</div>
 						<div class='p-4 text-lg'>
-							{doc.subtitle}
+							{data.subtitle}
 						</div></section>
 					<footer class="card-footer flex justify-center bg-black rounded-none rounded-b-lg pl-1 py-2"><a class="btn rounded-md variant-filled-success py-1 text-black" href={trail.url}>{start[$locale]}</a></footer>
 				</div>	
