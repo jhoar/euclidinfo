20481652aaee2b22a5cf2402fd01ef7ad8348874
jhoar <johnhoar@yahoo.co.uk>
2023-05-30 09:05:25 +0200

    Refactored info route

--- src/routes/info/[topic]/+page.ts
+++ src/routes/info/[topic]/+page.ts
@@ -2,11 +2,19 @@ import type { PageLoad } from './$types';
 
 export const load = ( async ({ fetch, params }) => {
 
+    const jsonFile = `\\data\\info\\info.json`
+
     // Load from /static
-    const jsonresp = await fetch(`\\info\\${params.topic}.json`);
+    const jsonresp = await fetch(jsonFile);
     const jsondata = await jsonresp.json(); 
-    const htmlresp = await fetch(`\\info\\${params.topic}.html`);
-    const htmldata = await htmlresp.text(); 
-    
-    return {json: jsondata, html: htmldata}
+    const itemData = jsondata[params.topic]['data'];
+
+    for (var item of itemData) {
+        const htmlFile = `\\data\\info\\${item['lang']}\\${params.topic}.html`;
+//        const htmlresp = await fetch(htmlFile); // FIX LANG
+        const htmlresp = await fetch(`\\data\\info\\en\\${params.topic}.html`); 
+        item['html'] = await htmlresp.text(); 
+    }
+
+    return {json: itemData}
 }) satisfies PageLoad;
