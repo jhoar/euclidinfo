dbb9ff3fb22e923bb7ba19df16121cbc2518db28
jhoar <johnhoar@yahoo.co.uk>
2023-05-31 06:26:39 +0200

    Translate pages

--- src/routes/trail/[topic]/[item]/+page.ts
+++ src/routes/trail/[topic]/[item]/+page.ts
@@ -2,17 +2,15 @@ import type { PageLoad } from './$types';
 
 export const load = ( async ({ fetch, params }) => {
 
-    const jsonFile = `\\data\\trail\\${params.topic}.json`
-
     // Load from /static
+    const jsonFile = `\\data\\trail\\${params.topic}.json`
     const jsonresp = await fetch(jsonFile);
     const jsondata = await jsonresp.json(); 
-    const itemData = jsondata[params.item]['data'];
 
+    const itemData = jsondata[params.item]['data'];
     for (var item of itemData) {
         const htmlFile = `\\data\\trail\\${params.topic}\\${item['lang']}\\${params.item}.html`;
-//        const htmlresp = await fetch(htmlFile); // FIX LANG
-        const htmlresp = await fetch(`\\data\\trail\\${params.topic}\\en\\${params.item}.html`); 
+        const htmlresp = await fetch(htmlFile);
         item['html'] = await htmlresp.text(); 
     }
 
