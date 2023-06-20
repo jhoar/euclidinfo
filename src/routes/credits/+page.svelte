c6ebf927f9142ac0cb32432a8b56ffc892e99ad9
jhoar <johnhoar@yahoo.co.uk>
2023-06-20 20:08:57 +0200

    Add credits

---
+++ src/routes/credits/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Navigation from '$lib/Navigation.svelte';
+</script>
+
+<Navigation left="" right=""/>
+
+<h1 class="text-white text-2xl pb-2"><span class="bg-red-600">CREDITS</span></h1>
+<ul class="list-disc">
+    <li>Discover Euclid: <b>ESA</b></li>
+</ul>
+
+<style>
+    :global(.prose p) {
+        font-family: 'Notes';
+    }
+
+    h1 {
+        font-family: "Notes Bold";
+    }
+</style>
