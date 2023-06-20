54cc882c9d97d72cca8f02ac039986588ca65e96
jhoar <johnhoar@yahoo.co.uk>
2023-06-20 22:59:38 +0200

    Update credits

--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -7,6 +7,8 @@
 <h1 class="text-white text-2xl pb-2"><span class="bg-red-600">CREDITS</span></h1>
 <ul class="list-disc">
     <li>Discover Euclid: <b>ESA</b></li>
+    <li>Built with : <a href="https://kit.svelte.dev/"><b>SvelteKit</b></a></li>
+    <li>Translations: <a href="https://www.deepl.com/translator"><b>DeepL</b></a>. If the translation is terrible, blame AI. Don't worry, it won't be the last time.</li>
 </ul>
 
 <style>
