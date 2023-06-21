99d5019ceca9e7aee7ab91e663e787bfb926dd8d
John Hoar <johnhoar@yahoo.co.uk>
2023-06-21 14:46:12 +0200

    Add Science101 skeleton

--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -7,6 +7,7 @@
 <h1 class="text-white text-2xl pb-2"><span class="bg-red-600">CREDITS</span></h1>
 <ul class="list-disc">
     <li>Discover Euclid: Euclid Launch Kit</li>
+    <li>Euclid Science 101: sci.esa.int</li>
     <li>Built with : <a href="https://kit.svelte.dev/">SvelteKit</a>, <a href="https://tailwindcss.com/TailwindCSS">TailwindCSS</a>, <a href="https://www.skeleton.dev/">Skeleton</a>, <a href="https://heroicons.com/">Heroicons</a></li>
     <li>Translations: <a href="https://www.deepl.com/translator">DeepL</a>. If the translation is terrible, blame AI. Don't worry, it won't be the last time.</li>
 </ul>
