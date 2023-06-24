f1eba9f8a25947fbdc772f9e6605337a3199e989
John Hoar <johnhoar@yahoo.co.uk>
2023-06-24 17:59:39 +0200

    Add share route

---
+++ src/routes/share/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import Navigation from '$lib/Navigation.svelte';
+    import { locale } from '$lib/Store';
+
+    let header : { [lang: string]: string } = {
+		'en' : 'Share this!',
+		'fr' : 'Partagez ce message !',
+		'es' : 'Compártelo!',
+		'de' : 'Teilen Sie das!',
+		'it' : 'Condividete questo!',
+		'nl' : 'Deel dit!'
+	};
+
+
+</script>
+
+<Navigation left={""} right={""}/>
+
+<div class="container pt-16 mx-auto flex justify-center items-center">
+    <h1 class="h1 text-white flex justify-center py-2">{header[$locale]}</h1>
+</div>
+<div class="container pt-16 mx-auto flex justify-center items-center">
+    <img alt='QR code' src='/qr.jpg' />
+</div>
+<style>
+    :global(.prose p) {
+        font-family: 'Notes';
+    }
+
+    h1 {
+        font-family: "Notes Bold";
+    }
+</style>
