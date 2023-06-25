e647b851d10018bde3109aefed4b08c36c39108a
jhoar <johnhoar@yahoo.co.uk>
2023-06-25 10:22:04 +0200

    change file name

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,5 +1,5 @@
 export async function load ( { fetch } ) {
-    const resp = await fetch(`\\data.json`);
+    const resp = await fetch(`\\card.json`);
     const appdata = await resp.json(); 
     return { appdata }
 }
