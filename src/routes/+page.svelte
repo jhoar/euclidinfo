5fd23ad002465622ec4d893a2218e6c3cad633b9
jhoar <johnhoar@yahoo.co.uk>
2023-06-25 10:20:27 +0200

    Update data.json to match trail json format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang='ts'>
 	import Clock from '$lib/Clock.svelte';
-import { locale } from '$lib/Store';
+	import { locale } from '$lib/Store';
     export let data;
 
 	let header : { [lang: string]: string } = {
@@ -28,19 +28,19 @@ import { locale } from '$lib/Store';
 <div class="container pt-16 mx-auto flex justify-center items-center">
 	<div class="imgbckspace-y-5 w-96">
 		<h1 class="h1 text-white flex justify-center py-2">{header[$locale]}</h1>
-		{#each data.appdata.trails as trail}
-			{#each trail.data as data} 
+		{#each data.appdata as card}
+			{#each card.data as data} 
 				{#if data.lang === $locale}
 				<div class="card m-2">
 					<header class="card-header variant-filled-tertiary rounded-none rounded-t-lg text-white text-2xl py-3">{(data.title).toUpperCase()}</header>
 					<section class="bg-black">
 						<div>
-							<img src={trail.image} alt={data.title}/>
+							<img src={card.image} alt={data.title}/>
 						</div>
 						<div class='p-4 text-lg'>
 							{data.subtitle}
 						</div></section>
-					<footer class="card-footer flex justify-center bg-black rounded-none rounded-b-lg pl-1 py-2"><a class="btn rounded-md variant-filled-success py-1 text-black" href={trail.url}>{start[$locale]}</a></footer>
+					<footer class="card-footer flex justify-center bg-black rounded-none rounded-b-lg pl-1 py-2"><a class="btn rounded-md variant-filled-success py-1 text-black" href={card.url}>{start[$locale]}</a></footer>
 				</div>	
 				{/if}
 			{/each}
