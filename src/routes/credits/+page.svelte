1b99ed1fd452683f31c17644b05e8e56fe580e93
John Hoar <johnhoar@yahoo.co.uk>
2023-06-26 08:10:50 +0200

    Update credits

--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -9,7 +9,7 @@
     <li>Discover Euclid: Euclid Launch Kit</li>
     <li>Euclid Science 101: sci.esa.int</li>
     <li>Built with : <a href="https://kit.svelte.dev/">SvelteKit</a>, <a href="https://tailwindcss.com/TailwindCSS">TailwindCSS</a>, <a href="https://www.skeleton.dev/">Skeleton</a>, <a href="https://heroicons.com/">Heroicons</a></li>
-    <li>Translations: <a href="https://www.deepl.com/translator">DeepL</a>. If the translation is terrible, blame AI. Don't worry, it won't be the last time.</li>
+    <li>Translations: <a href="https://www.deepl.com/translator">DeepL</a>.</li>
 </ul>
 
 <style>
